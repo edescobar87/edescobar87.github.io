149ec2a9e0ca183c25502208db6323bd2a3f6dba
edescobar87 <esc16001@byui.edu>
2021-05-03 11:14:54 -0600

    menu

--- menu-example/js/menu-inclass.js
+++ menu-example/js/menu-inclass.js
@@ -1,8 +1,8 @@
 window.addEventListener('load', ()=> {
-    const hambutton = document.querySelector('ham');
+    const hambutton = document.querySelector('.ham');
     const mainnav = document.querySelector('#navigation');
 
     hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
-
+        // 
     // window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
 });
