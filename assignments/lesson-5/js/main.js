1260e210b44dbc04a527d0398583f419d2972883
edescobar87 <esc16001@byui.edu>
2021-05-24 06:49:12 -0600

    js less 5

--- assignments/lesson-5/js/main.js
+++ assignments/lesson-5/js/main.js
@@ -8,12 +8,21 @@ window.addEventListener('load', ()=> {
 });
 
 // if its friday it will hopefull display ananouncement about pancakes in the park oh baby I hope this owrks if not then.... that sucks//
-const pancakes = document.querySelector('#pancakes');
-if (currentDate.getDay() == 5) {
-    pancakes.style.display = 'block';
-}
-else {
-    pancakes.style.display = 'none';
-}
+// const pancake = document.querySelector('pancakes');
+// if (currentDate.getDay() == 5) {
+//     pancake.style.display = 'block';
+// }
+// else {
+//     pancakes.style.display = 'none';
+// }
+
+const pancake = document.getElementById('pancakes');
+    if (d.getDay() == 5) { //5 is Friday
+        
+        pancake.style.display = "block";
+    }
+    else {
+        pancake.style.display = "none";
+    }
 
 
