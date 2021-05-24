5b328102dc09f8381a56e982375d63c13d60ffe6
edescobar87 <esc16001@byui.edu>
2021-05-24 08:13:06 -0600

    js windchill

--- assignments/lesson-6/js/windchill.js
+++ assignments/lesson-6/js/windchill.js
@@ -1,11 +1,19 @@
-function getWindChillValue () {
+// function getWindChillValue () {
 
-    var f;
-    var t = 76;
-    var s = 5;
-    var e = Math.pow (s, 0.16);
+//     var f;
+//     var t = 76;
+//     var s = 5;
+//     var e = Math.pow (s, 0.16);
     
-    f = 35.74 + (0.6215 * t) - (35.75 * e) + (0.4275 * t * e);
+//     f = 35.74 + (0.6215 * t) - (35.75 * e) + (0.4275 * t * e);
     
-    document.getElementById("windChillValue").innerHTML = getWindChillValue;
-    }
+//     document.getElementById("windChillValue").innerHTML = getWindChillValue;
+//     }
+
+
+var temp= 57;
+var wSpeed= 15;
+var windChillValue= (35.74 + (0.6215 * temp))-(35.75 * Math.pow(wSpeed,0.16)) + (0.4275*temp*Math.pow(wSpeed,0.16));
+
+var windChillValue= Math.round(windChillValue);
+document.getElementById("windChillValue").innerHTML= windChillValue;
