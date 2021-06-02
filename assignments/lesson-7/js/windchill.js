7c1911dd0cc2e3ff28bbb79b64605e9f236bfd52
edescobar87 <esc16001@byui.edu>
2021-06-02 06:47:01 -0600

    huge

---
+++ assignments/lesson-7/js/windchill.js
@@ -0,0 +1,20 @@
+// function getWindChillValue () {
+
+//     var f;
+//     var t = 76;
+//     var s = 5;
+//     var e = Math.pow (s, 0.16);
+    
+//     f = 35.74 + (0.6215 * t) - (35.75 * e) + (0.4275 * t * e);
+    
+//     document.getElementById("windChillValue").innerHTML = getWindChillValue;
+//     }
+
+
+var temp= 54.1;
+var wSpeed= 12;
+var windChillValue= (35.74 + (0.6215 * temp))-(35.75 * Math.pow(wSpeed,0.16)) + (0.4275*temp*Math.pow(wSpeed,0.16));
+
+var windChillValue= Math.round(windChillValue);
+document.getElementById("windChillValue").innerHTML= windChillValue;
+
