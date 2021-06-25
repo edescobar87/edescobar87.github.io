946d554704221ba099bdac5b323daf3166fc08c7
edescobar87 <esc16001@byui.edu>
2021-06-25 17:21:36 -0600

    yeet

--- assignments/lesson-10/js/api.js
+++ assignments/lesson-10/js/api.js
@@ -1,27 +1,37 @@
- fetch('https://api.openweathermap.org/data/2.5/weather?&id=5604473&units=imperial&APPID=c837d202aac77367d90725ec4b9c7ce2')
+async function getWeather() {
+    const url = "https://api.openweathermap.org/data/2.5/weather?&id=5604473&units=imperial&appid=ec88a9a3a0d62c0053ddeb7408e1bda3";
 
-    .then(function (response) {
+    const response = await fetch(url);
+
+    if (response.status == 200) {
         return response.json();
-    })
-    .then(function (jsObject) {
+    } else {
+        throw new Error("No weather found + " + response.status);
+    }
+}
 
-        console.log(jsObject);
+    // .then(function (response) {
+    //     return response.json();
+    // })
+    // .then(function (jsObject) {
 
-        document.getElementById('des').innerHTML = jsObject.weather[0].description;
-        document.getElementById('des1').innerHTML = jsObject.weather[0].description;
-        document.getElementById('temp').innerHTML = jsObject.main.temp;
-        document.getElementById('hum').innerHTML = jsObject.main.humidity;
-        document.getElementById('windspeed').innerHTML = jsObject.wind.speed;
+    //     console.log(jsObject);
+
+        // document.getElementById('des').innerHTML = jsObject.weather[0].description;
+        // document.getElementById('des1').innerHTML = jsObject.weather[0].description;
+        // document.getElementById('temp').innerHTML = jsObject.main.temp;
+        // document.getElementById('hum').innerHTML = jsObject.main.humidity;
+        // document.getElementById('windspeed').innerHTML = jsObject.wind.speed;
 
 
-        let imagesrc = 'http://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';
-        let desc = jsObject.weather[0].description;
-        document.getElementById('weatherimage').innerHTML = imagesrc;
-        document.getElementById('icon').setAttribute('src', imagesrc);
-        document.getElementById('icon').setAttribute('alt', desc);
+        // let imagesrc = 'http://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';
+        // let desc = jsObject.weather[0].description;
+        // document.getElementById('weatherimage').innerHTML = imagesrc;
+        // document.getElementById('icon').setAttribute('src', imagesrc);
+        // document.getElementById('icon').setAttribute('alt', desc);
         
 
-    });
+    
 
 
        /*****************
--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 async function getWeather() {
     const url = "https://api.openweathermap.org/data/2.5/forecast?zip=83440,us&units=imperial&appid=be8753d36dab0642baf37a7f8906882f";
-
     const response = await fetch(url);
 
     if (response.status == 200) {
@@ -10,13 +9,25 @@ async function getWeather() {
     }
 }
 
-// list[0].main.temp
-// 0, 8, 16, 24, 32, 39
+function daysOfWeek(date) {
+
+    var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+
+    return weekday;
+}
 
 function weather() {
     const w = getWeather()
-        .then(function(weather) {
-            const date = new Date(); // Make this dynamic (Pull the next couple dates instead of hard coding them)
+        .then(function (weather) {
+            // const date = new Date(); // Make this dynamic (Pull the next couple dates instead of hard coding them)
+            // console.log(date);
 
             console.log(weather);
             let currentWeather = document.querySelector('#current-weather');
@@ -31,8 +42,57 @@ function weather() {
             dayThree.textContent = Math.floor(weather.list[24].main.temp);
             dayFour.textContent = Math.floor(weather.list[32].main.temp);
             dayFive.textContent = Math.floor(weather.list[39].main.temp);
-            
+
+            // Dynamically change city name
+            let cityName = document.querySelector("#city-name");
+            cityName.textContent = weather.city.name;
+
+            let desc = document.querySelector("#current-weather-desc");
+            desc.textContent = capitalizeFirstLetter(weather.list[0].weather[0].description);
+
+            var dayNum = 1;
+            var week = daysOfWeek();
+            for (let i = 0; i < weather.list.length; i++) {
+                if (i == 8 || i == 16 || i == 24 || i == 32 || i == 39) {
+                    let day = document.querySelector('#weekday' + dayNum);
+                    var day1 = new Date(weather.list[i].dt_txt);
+                    day1.getDay();
+                    day.textContent = week[day1.getDay()];
+
+                   
+
+                    let icon = document.querySelector('#icon' + dayNum);
+                    switch (weather.list[i].weather[0].main) {
+                        case 'Clear':
+                            icon.setAttribute('class', 'wi wi-day-sunny')
+                            break;
+                        case 'Thunderstorm':
+                            icon.setAttribute('class', 'wi wi-thunderstorm')
+                            break;
+                        case 'Drizzle':
+                            icon.setAttribute('class', 'wi wi-showers')
+                            break;
+                        case 'Rain':
+                            icon.setAttribute('class', 'wi wi-rain')
+                            break;
+                        case 'Snow':
+                            icon.setAttribute('class', 'wi wi-snow')
+                            break;
+                        case 'Clouds':
+                            icon.setAttribute('class', 'wi wi-cloud')
+                            break;
+                        default:
+                    }
+
+                    dayNum++;
+                }
+            }
+
         });
+
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+    }
 }
 
 window.addEventListener('load', weather());
