7ca80f7b4407b58c4df6f5b2b066f10c0f1ab896
edescobar87 <esc16001@byui.edu>
2021-06-26 17:10:30 -0600

    up

--- assignments/lesson-10/js/weatherapi.js
+++ assignments/lesson-10/js/weatherapi.js
@@ -29,18 +29,27 @@
             let count = 0;
             var dayNames = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
             jsObject.list.forEach(element => {
-                // check for same time each day
+                /**************************
+                 CHECKING TIME FOR EACH DAY
+                 **************************/
                 if(element.dt_txt.includes('18:00:00')){
-                    // set lable to correct day
-                    document.querySelector('#forecastTempDays' + count).textContent = (dayNames[new Date(element.dt_txt.slice(0, 10)).getDay()]);
+
+                    /**************************************
+                      SETTING THE LABLE FOR THE CORRECT DAY
+                     **************************************/ 
+                document.querySelector('#forecastTempDays' + count).textContent = (dayNames[new Date(element.dt_txt.slice(0, 10)).getDay()]);
     
-                    // set temp for each day
-                    document.querySelector('#temp' + count).textContent = Math.round(element.main.temp_max) + " °F";
+                    /***********************************
+                     SETTING UP TEMPERATURE FOR EACH DAY
+                     ***********************************/ 
+                document.querySelector('#temp' + count).textContent = Math.round(element.main.temp_max) + " °F";
     
-                    // set icon for each day
+                    /*******************************************
+                     SETTING UP THE ICONS FOR EACH OF THERE DAYS 
+                     *******************************************/ 
                    
-                    document.querySelector('#weatherimg' + count).setAttribute('src', "http://openweathermap.org/img/wn/" + element.weather[0].icon + ".png")
-                    count = count + 1;
+                document.querySelector('#weatherimg' + count).setAttribute('src', "http://openweathermap.org/img/wn/" + element.weather[0].icon + "@2x.png")
+                count = count + 1;
                 };
             });
         });
