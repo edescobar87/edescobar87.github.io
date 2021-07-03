d676dddc45c38ad6c4bdaf32cab689856a113024
edescobar87 <esc16001@byui.edu>
2021-07-03 15:52:19 -0600

    temple

--- assignments/lesson-12/js/button.js
+++ assignments/lesson-12/js/button.js
@@ -1,16 +1,25 @@
-window.onscroll = function() {scrollFunction()};
+// window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
-    document.getElementById("smallViewBtoTop").style.display = "block";
-  } else {
-    document.getElementById("smallViewBtoTop").style.display = "none";
-  }
+// function scrollFunction() {
+//   if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+//     document.getElementById("smallViewBtoTop").style.display = "block";
+//   } else {
+//     document.getElementById("smallViewBtoTop").style.display = "none";
+//   }
   
-}
+// }
 
 
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+// function topFunction() {
+//   document.body.scrollTop = 0;
+//   document.documentElement.scrollTop = 0;
+// }
+
+window.addEventListener('load', ()=> {
+  const hambutton = document.querySelector('.ham');
+  const mainnav = document.querySelector('#navigation');
+
+  hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+      // 
+  // window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+});
--- assignments/lesson-12/js/toggmenu.js
+++ assignments/lesson-12/js/toggmenu.js
@@ -1,3 +1,4 @@
 function toggleMenu() {
-    document.getElementById("primaryNav").classList.toggle('hide');
+    document.getElementById("mainNav").classList.toggle('hide');
 }
+
